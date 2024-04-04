6833fa1deb9ac5ecd4cc40d90773e4af22729e39
Cristian Ramirez <cristianramirezgt@gmail.com>
2024-04-04 00:20:27 -0600

    feat: Add logic to login the user

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -36,15 +36,40 @@ const createUser = async(req: Request, res: Response) => {
   } 
 }
 
-const loginUser = (req: Request, res: Response) => {
+const loginUser = async(req: Request, res: Response) => {
   const { email, password } = req.body;
 
-  res.status(200).json({
-    ok: true,
-    msg: 'login',
-    email,
-    password,
-  })
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'User does not exists.'
+      });
+    }
+
+    const validPassword = bcrypt.compareSync(password, user.password);
+
+    if (!validPassword) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'Not valid credentials',
+      })
+    }
+
+    res.status(201).json({
+      ok: true,
+      uid: user.id,
+      name: user.name,
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Failed, please reach the admin.'
+    })
+  }
 }
 
 const renewToken = (req: Request, res: Response) => {
