74bdc071edaa5c1ca5ddf738e02e144e30137ab3
Cristian Ramirez <cristianramirezgt@gmail.com>
2024-04-03 21:29:28 -0600

    feat: Add custom middleware validate-fields , to avoid repeted code on controller

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,17 +1,7 @@
 import {  Request, Response } from 'express';
-import { validationResult } from 'express-validator';
 
 const createUser = (req: Request, res: Response) => {
   const { name, email, password } = req.body;
-
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      ok: false,
-      errors: errors.mapped()
-    })
-  }
 
   res.status(201).json({
     ok: true,
@@ -24,15 +14,6 @@ const createUser = (req: Request, res: Response) => {
 
 const loginUser = (req: Request, res: Response) => {
   const { email, password } = req.body;
-
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      ok: false,
-      errors: errors.mapped(),
-    })
-  }
 
   res.status(200).json({
     ok: true,
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { check } from 'express-validator';
 
 import { createUser, loginUser, renewToken } from '../controllers/auth';
+import { validateFields } from '../middlewares/validate-fields';
 
 const router = Router();
 
@@ -10,7 +11,8 @@ router.post(
   [
     check('name', 'Name is required').not().isEmpty(),
     check('email', 'Email is required').isEmail(),
-    check('password', 'Password length must be greather than 6 characters').isLength({ min: 6 })
+    check('password', 'Password length must be greather than 6 characters').isLength({ min: 6 }),
+    validateFields
   ],
   createUser
 );
@@ -19,7 +21,8 @@ router.post(
   '/',
   [
     check('email', 'Email is not valid').isEmail(),
-    check('password', 'Password length must be greather than 6 characters').isLength({ min: 6 })
+    check('password', 'Password length must be greather than 6 characters').isLength({ min: 6 }),
+    validateFields
   ],
   loginUser
 );
